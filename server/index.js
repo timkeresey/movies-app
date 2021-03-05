509d255afd184a1f5da05f181be7eff161843c98
Tim Keresey <tim.keresey@gmail.com>
2021-03-04 19:11:41 -0700

    require database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+const db = require('./db')
+
 const app = express()
 const apiPort = 3000
 
