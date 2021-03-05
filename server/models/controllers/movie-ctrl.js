16bee90f1162746ad4c2e00990dee5a10b7d9372
Tim Keresey <tim.keresey@gmail.com>
2021-03-04 19:21:51 -0700

    add delete movie controller

--- server/models/controllers/movie-ctrl.js
+++ server/models/controllers/movie-ctrl.js
@@ -36,4 +36,49 @@ updateMovie = async (req, res) => {
             error: 'You must provide a body to update',
         })
     }
+
+    Movie.findOne({ _id:req.params.id }, (err, movie) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: "Movie not found!",
+            })
+        }
+        movie.name = body.name
+        movie.time = body.time
+        movie.rating = body.rating
+        movie
+            .save()
+            .then(() => {
+                return res.status(200).json({
+                    success: true,
+                    id: movie._id,
+                    message: 'Movie updated!',
+                })
+            })
+            .catch(error => {
+                return res.status(404).json({
+                    error,
+                    message: 'Movie not updated!',
+                })    
+            })
+    })
 }
+
+deleteMovie = async (req, res) => {
+    await Movie.findOneAndDelete({ _id: req.params.id }, (err, movie) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+
+        if (!movie) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Movie not found` })
+        }
+
+        return res.status(200).json({ success: true, data: movie })
+    }).catch(err => console.log(err))
+}
+
+
